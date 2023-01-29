98d6aaf39389bdbb25226352a521fa6958ec7eac
aji prasetyo <ajiprasetyo@digitalsekuriti.id>
2023-01-29 23:25:40 +0700

    expect function

--- test/sample_test.dart
+++ test/sample_test.dart
@@ -1,12 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
 
-
 /// Untuk menjalankan unit test. bisa menggunakan "dart test" pada terminal
 void main() {
   test(
     "Unit Test",
     () {
       /// Isi Unit Test nya
+      var respone = sayHello("Dart");
+      expect(respone, "Hello dart");
     },
   );
+}
+
+String sayHello(String name) {
+  return "Hello $name";
 }
