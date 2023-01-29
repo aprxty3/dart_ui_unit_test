3ed6ab5f548c619866b7b89bef3f414cba13cbb4
aji prasetyo <ajiprasetyo@digitalsekuriti.id>
2023-01-29 23:34:55 +0700

    matcher

--- test/sample_test.dart
+++ test/sample_test.dart
@@ -7,11 +7,34 @@ void main() {
     () {
       /// Isi Unit Test nya
       var respone = sayHello("Dart");
-      expect(respone, "Hello dart");
+      expect(respone, "Hello Dart");
+    },
+  );
+  test(
+    "Unit Test with Matcher",
+    () {
+      /// Isi Unit Test nya
+      expect(sayHello("Dwi"), endsWith("Dwi"));
+      expect(sayHello("Dwi"), startsWith("Hello"));
+      expect(sayHello("Dwi"), equalsIgnoringCase("hello dwi"));
+      expect(sayHello("Dwi"), isA<String>());
+    },
+  );
+  test(
+    "Unit Test with Matcher",
+    () {
+      /// Isi Unit Test nya
+      expect(sum(1, 1), equals(2));
+      expect(sum(1, 1), greaterThan(1));
+      expect(sum(1, 1), lessThan(10));
     },
   );
 }
 
 String sayHello(String name) {
   return "Hello $name";
+}
+
+int sum(int a, int b) {
+  return a + b;
 }
