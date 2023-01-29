d07982c0aefdacdacc4d223b35a19bfe39946d5c
aji prasetyo <ajiprasetyo@digitalsekuriti.id>
2023-01-29 23:18:41 +0700

    how to run dart test

---
+++ test/sample_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+
+/// Untuk menjalankan unit test. bisa menggunakan "dart test" pada terminal
+void main() {
+  test(
+    "Unit Test",
+    () {
+      /// Isi Unit Test nya
+    },
+  );
+}
