ea3e294f34a57731cc9a9ff2fa805a6345c3b435
aji prasetyo <ajiprasetyo@digitalsekuriti.id>
2023-03-04 16:00:02 +0700

    tear down test function

---
+++ test/teardown_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  var data = "Aji";
+  setUp(() => data = "Aji");
+
+  tearDown(
+    () => print(data),
+  );
+
+  group("Test Setup Test", () {
+    test('test firts', () {
+      data = "$data Prasetyo";
+      expect(data, equals("Aji Prasetyo"));
+    });
+    test('test second', () {
+      data = "$data setyo";
+      expect(data, equals("Aji setyo"));
+    });
+  });
+}
