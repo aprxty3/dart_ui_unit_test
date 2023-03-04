82a62196fec142303edc7ecc66d9394b36e51409
aji prasetyo <ajiprasetyo@digitalsekuriti.id>
2023-03-04 16:44:01 +0700

    platform selector test function

---
+++ test/platform_selector_test.dart
@@ -0,0 +1,14 @@
+@TestOn("mac-os || windows || dart-vm")
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("Test Sum", () {
+    test(
+      "positive",
+      () => expect(sum(1, 2), equals(3)),
+    );
+    test("negative", () => expect(sum(10, -5), equals(5)), testOn: 'mac-os');
+  });
+}
+
+int sum(int a, int b) => a + b;
