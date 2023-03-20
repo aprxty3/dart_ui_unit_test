7db71c9b01f0ed3c59eca8b812bce123ab68707e
aprxty3 <ajiprxty3@gmail.com>
2023-03-20 22:49:01 +0700

    build mock object

---
+++ test/mock_object_test.dart
@@ -0,0 +1,7 @@
+import 'package:dart_ui_unit_test/book.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateNiceMocks([MockSpec<BookRepository>()])
+import 'mock_object_test.mocks.dart';
+
+void main() {}
