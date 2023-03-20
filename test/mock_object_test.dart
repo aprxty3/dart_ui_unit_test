970859a2b8f942c54be5df7b940792d9a0dfb415
aprxty3 <ajiprxty3@gmail.com>
2023-03-20 22:53:12 +0700

    try mock

--- test/mock_object_test.dart
+++ test/mock_object_test.dart
@@ -1,7 +1,26 @@
 import 'package:dart_ui_unit_test/book.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 
 @GenerateNiceMocks([MockSpec<BookRepository>()])
 import 'mock_object_test.mocks.dart';
 
-void main() {}
+void main() {
+  group(
+    'Book Service',
+    () {
+      var bookRepo =
+          MockBookRepository(); //If Use This, the mocking could be success
+      var bookRepos =
+          BookRepository(); //If Use This, the mocking could be error
+      var bookSevice = BookService(bookRepo);
+
+      test(
+        'save new book must success',
+        () {
+          bookSevice.save('1', 'Tutor', 100);
+        },
+      );
+    },
+  );
+}
